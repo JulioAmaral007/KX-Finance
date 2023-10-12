ae9eb39c33c32bfa83472a728f1d4c36de63afba
Julio Cezar <captaingamergroot@gmail.com>
2023-10-12 09:43:29 -0300

    ✨ Create component Header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,6 +1,15 @@
 'use client'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 interface HeaderProps {
   title: string
@@ -29,7 +38,27 @@ export function Header({ title }: HeaderProps) {
       )}
 
       {status === 'authenticated' && data.user && (
+        <div className="border-primary relative flex items-center gap-3 rounded-full border border-solid py-2 px-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center gap-2">
+              Open
+            </DropdownMenuTrigger>
 
+            <DropdownMenuContent className="mr-3 mt-2 bg-primary-foreground">
+              <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="text-primary border-border cursor-pointer border-b border-solid pb-2 text-sm font-semibold">
+                <Link href="/profile">Meu perfil</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="text-primary cursor-pointer pt-2 text-sm font-semibold"
+                onClick={handleSignOut}
+              >
+                Sair
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       )}
     </div>
   )
